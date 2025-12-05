109b78b956224229352bb7222b9872d6803f70d0
Harsh Patel <harshp.avidclan@gmail.com>
2025-12-05 15:55:11 +0530

    Navigation add

--- src/Navigation/BuyerStackNavigator.tsx
+++ src/Navigation/BuyerStackNavigator.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { BuyerTabNavigator } from './BuyerTabNavigator';
 import { BuyerStackParamList, Routes } from '../Types/Navigation';
+import { BuyerPostListingPropertyScreen } from '../components/Buyer/BuyerPostListingPropertyScreen/BuyerPostListingPropertyScreen';
+import { BuyerPostListingVehicleScreen } from '../components/Buyer/BuyerPostListingVehicleScreen/BuyerPostListingVehicleScreen';
 
 const Stack = createNativeStackNavigator<BuyerStackParamList>();
 
@@ -14,6 +16,15 @@ export const BuyerStackNavigator: React.FC = () => {
       }}
     >
       <Stack.Screen name={Routes.BUYER_TABS} component={BuyerTabNavigator} />
+      <Stack.Screen
+        name={Routes.BUYER_POST_LISTING_PROPERTY}
+        component={BuyerPostListingPropertyScreen}
+      />
+
+      <Stack.Screen
+        name={Routes.BUYER_POST_LISTING_VEHICLE}
+        component={BuyerPostListingVehicleScreen}
+      />
     </Stack.Navigator>
   );
 };
